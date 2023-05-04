5431b3f123e683767eb6d7e8a8983567483d6280
Moiseis-Cruz <moizzito@gmail.com>
2023-05-04 18:26:06 -0300

    Comocei o arquivo js.

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const btnOption = document.getElementById("toggle")
+
+btnOption.addEventListener("change", function(){
+    const containerCards = document.getElementsByClassName("container-cards")
+
+    const sectionActived = document.querySelector(".actived")
+
+    const containerActived = sectionActived.classList.contains("active")
+
+    
+})
