772d295247e3398e7ce09635b51bb6d0db7b50bf
moizzito@gmail.com <moizzito@gmail.com>
2023-08-10 18:28:47 -0300

    Refiz o código.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,20 @@
+const btnToggle = document.getElementById("toggle")
+
+
+btnToggle.addEventListener("click", () => {
+    const plans = document.querySelectorAll(".container-cards")
+
+    plans.forEach((listPlans) => {
+        const sectionActived = document.querySelector(".actived")
+
+        sectionActived.classList.remove("actived")
+        
+        listPlans.classList.add("actived")
+    })
+})
+
+
+/*
 const btnToggle = document.querySelector("#toggle")
 
 const containerCards = document.querySelectorAll(".container-cards")
@@ -15,3 +32,4 @@ function desativarSecao() {
 
     sectionActived.classList.remove("actived")
 }
+*/
