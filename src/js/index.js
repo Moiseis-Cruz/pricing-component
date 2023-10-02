be496661ce4f5822357828d931bf788654e3279a
Moiseis Cruz <moizzito@gmail.com>
2023-10-01 21:05:06 -0300

    Criei o evento de click.

--- src/js/index.js
+++ src/js/index.js
@@ -3,20 +3,21 @@ const btnToggle = document.getElementById("toggle")
 const containerCards = document.querySelectorAll(".container-cards")
 
 const containerActived = document.querySelector(".actived.container-cards")
-/*
 
 btnToggle.addEventListener("click", (event, index) => {
     
     if(btnToggle.checked === true){
         containerActived.classList.remove("actived")
-
+        
         console.log(event);
         
         containerCards[1].classList.add("actived")
     }else{
         containerActived.classList.add("actived")
-
+        
         containerCards[1].classList.remove("actived")
     }
 })
+
+/*
 */
