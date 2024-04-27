fc1e5d2078bbabfef127737fa098ab1c061f7c57
Moiseis Cruz <moizzito@gmail.com>
2024-04-27 12:37:24 -0300

    I added semicolons to organize.

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,18 @@
-const btnToggle = document.getElementById("toggle")
+const btnToggle = document.getElementById("toggle");
 
-const containerCards = document.querySelectorAll(".container-cards")
+const containerCards = document.querySelectorAll(".container-cards");
 
-const containerActived = document.querySelector(".actived.container-cards")
+const containerActived = document.querySelector(".actived.container-cards");
 
 btnToggle.addEventListener("click", () => {
     
     if(btnToggle.checked === true){
-        containerActived.classList.remove("actived")
+        containerActived.classList.remove("actived");
         
-        containerCards[1].classList.add("actived")
+        containerCards[1].classList.add("actived");
     }else{
-        containerActived.classList.add("actived")
+        containerActived.classList.add("actived");
         
-        containerCards[1].classList.remove("actived")
-    }
-})
+        containerCards[1].classList.remove("actived");
+    };
+});
